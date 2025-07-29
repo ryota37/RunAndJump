fcf3885feffdd5790959979e29b7f4c86c663c2a
ryota37 <ryota.nakahama@access-company.com>
2025-07-29 20:30:58 +0900

    Impl: player and auto scroll

--- RunAndJump/Main.cpp
+++ RunAndJump/Main.cpp
@@ -3,13 +3,17 @@
 void Main()
 {
 
-	Rect background{ 0,-200,2400,800 };
+	Rect background{ 0,-200,7200,800 };
+	Circle player{ Scene::Center().x, 550, 30};
 
 	while (System::Update())
 	{
 		background.draw(Arg::left = Palette::Black, Arg::right = Palette::White);
+		player.draw(Palette::Red);
 
+		background.x -= 10;
 
+		// Debug
 		if (KeyLeft.pressed())
 		{
 			background.x += 10;
