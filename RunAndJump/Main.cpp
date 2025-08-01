d9d77de186c692984f7242adc1b939397548adf2
ryota37 <ryota.nakahama@access-company.com>
2025-08-01 22:02:18 +0900

    Impl: Jump

--- RunAndJump/Main.cpp
+++ RunAndJump/Main.cpp
@@ -1,17 +1,50 @@
 ﻿# include <Siv3D.hpp> // Siv3D v0.6.15
 
+bool isPlayerOnGround(const Circle& player)
+{
+	if (player.y + player.r >= 550) return true;
+	return false;
+}
+
+void gravity(Circle& player)
+{
+	if (!isPlayerOnGround(player)) player.y += 5;
+}
+
+void AutoScroll(Rect& background)
+{
+	background.x -= 10;
+}
+
+void PlayerJump(Circle& player, int& jumpFrame)
+{
+	if (isPlayerOnGround(player))
+	{
+		if (KeySpace.down() && jumpFrame <= 0) jumpFrame = 30;
+	}
+
+	if (jumpFrame > 0)
+	{
+		player.moveBy(0, -10);
+		jumpFrame--;
+	}
+}
+
 void Main()
 {
 
 	Rect background{ 0,-200,7200,800 };
 	Circle player{ Scene::Center().x, 550, 30};
+	int jumpFrame = 0;
 
 	while (System::Update())
 	{
 		background.draw(Arg::left = Palette::Black, Arg::right = Palette::White);
 		player.draw(Palette::Red);
 
-		background.x -= 10;
+		AutoScroll(background);
+		gravity(player);
+		PlayerJump(player, jumpFrame);
 
 		// Debug
 		if (KeyLeft.pressed())
@@ -22,8 +55,5 @@ void Main()
 		{
 			background.x -= 10;
 		}
-
-
-
 	}
 }
